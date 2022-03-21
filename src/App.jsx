5faaf3d9c66446d610cbe2244688019549852037
TonyPurple <tony.vanoni@outlook.com>
2022-03-21 06:46:19 -0700

    add bootstrap for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+      <strong>Oh snap! You got an error!</strong> 
+      <p> 
+        Change this and that and try again.
+      </p>
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
     </div>
-  );
+  )
 }
 
 export default App;
