b135a83c121c464590d9e0f40955c0d779fb6523
TonyPurple <tony.vanoni@outlook.com>
2022-03-21 06:50:03 -0700

    install react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
+import React, {Component} from 'react';
 import './App.css';
+import {NavLink} from 'react-router-dom';
 
-function App() {
-  return (
-    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-      <strong>Oh snap! You got an error!</strong> 
-      <p> 
-        Change this and that and try again.
-      </p>
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
-  )
+class App extends Component {
+  state = {
+    clubs: []
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          サッカー　Sakkaa
+          <nav>
+            <NavLink exact to='/'>CLUBS LIST</NavLink>
+            &nbsp;&nbsp;&nbsp;
+            <NavLink exact to='/add'>ADD CLUB</NavLink>
+          </nav>
+        </header>
+        <main>
+        <h1>test</h1>
+        </main>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App; 
